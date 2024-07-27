c407ca1326a2e778f755d88e4c8ceb4ca002d9b7
gsuke <54441258+gsuke@users.noreply.github.com>
2024-07-27 12:40:47 +0900

    feat: add biome

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,17 @@
-import React from 'react'
-import {createRoot} from 'react-dom/client'
-import './style.css'
-import App from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./style.css";
+import App from "./App";
 
-const container = document.getElementById('root')
+const container = document.getElementById("root");
+if (!container) {
+	throw new Error("root element not found");
+}
 
-const root = createRoot(container!)
+const root = createRoot(container);
 
 root.render(
-    <React.StrictMode>
-        <App/>
-    </React.StrictMode>
-)
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+);
